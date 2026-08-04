d138ad28283f0bb04a498f5fa2b5d6f982c66abe
AdamT2529 <profileytwow1@gmail.com>
2026-08-04 18:21:18 -0400

    imported api key to give vite access to code stored in env and loading text on screen

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 const date = document.querySelector("#datepicker").value;
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+document.querySelector("#app").innerHTML = "<p>loading...</p>";
