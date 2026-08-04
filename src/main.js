89090bf6555b64472d1dbdbb834fa2b6fdca697b
AdamT2529 <profileytwow1@gmail.com>
2026-08-04 18:50:55 -0400

    Bring the data to the site

--- src/main.js
+++ src/main.js
@@ -8,5 +8,6 @@ document.querySelector("#app").innerHTML = "<p>loading...</p>";
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`).
 then ( response => response.json()).then (data => {
-    console.log(data);
+    document.querySelector("#app").innerHTML= `
+    <h1>${data.title}</h1> `;
 })
